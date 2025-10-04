0166be52aa8bee2d894daeef641a99e558def095
João Marcos Simon Paulini <joaomarcossimonpaulini@Joaos-MacBook-Pro.local>
2025-10-03 22:33:04 -0300

    sds

---
+++ quiz/main.py
@@ -0,0 +1,10 @@
+def eh_primo(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+primos = [n for n in range(1, 1000000) if eh_primo(n)]
+print("Números primos de 1 a 100:", primos)
